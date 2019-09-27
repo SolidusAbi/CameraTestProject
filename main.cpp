516371a738ec88715e0fef1902b354e2c51ca3b1
Abian Hernandez <abianhernandez@outlook.es>
2019-09-27 16:37:40 +0100

    Application updated, using the new version of ThermalCameraLab

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <thermalcameralib.h>
+//#include <thermalcameralib.h>
+
+#include <ThermalCameraLib/TEQ1Camera.h>
+#include <ThermalCameraLib/CameraGLViewer.h>
 
 int main(int argc, char *argv[])
 {
-    mt4sd::ThermalCameraLib *camera = new mt4sd::ThermalCameraLib(mt4sd::Camera::I3_TE_Q1);
-    if (!camera->isConnected())
-        return -1;
+    //mt4sd::ThermalCameraLib *camera = new mt4sd::ThermalCameraLib(mt4sd::CameraType::I3_TE_Q1);
+//    if (!camera->isConnected())
+//        return -1;
+
+    mt4sd::Camera *camera = new mt4sd::TEQ1Camera();
 
     QApplication a(argc, argv);
     MainWindow w;
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <cameraglviewer.h>
+#include <ThermalCameraLib/CameraGLViewer.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
